dd5d86769df8512b508b0e14cb68f83101471dac
Simon-Hubert <s.hubert.mailpro@gmail.com>
2026-09-11 10:07:59 +0200

    [Add] VFX

--- Assets/_Project/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/_Project/Scripts/Main Menu/MainMenuManager.cs
@@ -1,11 +1,11 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    
-
 
     public void StartGame()
     {
@@ -15,7 +15,9 @@ public class MainMenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit(); // Ne fonctionne qu'en build
+#if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
+#endif
     }
 
 }
